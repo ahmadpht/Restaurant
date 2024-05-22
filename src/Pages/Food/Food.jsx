9762ff0790b1aafa6cd29712802ee415cb9c5c3a
ahmadpht <ahmadpirhayati7777@gmail.com>
2024-05-22 05:04:01 -0700

    component updated : Food.jsx

--- src/Pages/Food/Food.jsx
+++ src/Pages/Food/Food.jsx
@@ -5,24 +5,26 @@ import { useParams } from "react-router-dom";
 const Food = () => {
   const [meal, setMeal] = useState([]);
   const params = useParams();
+  const finalArr =[];
 
+  const ingredientArr = [];
+  const measureArr = [];
+  
   useEffect(() => {
     axios
-      .get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${params.id}`)
-      .then((res) => {
-        setMeal(res.data.meals[0]);
-      });
+    .get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${params.id}`)
+    .then((res) => {
+      setMeal(res.data.meals[0]);
+    });
   }, []);
-
-  const ingeredientArr = [];
-  const measureArr = [];
+  
   
   for (let item in meal) {
     if (item.includes("strIngredient")) {
       if (meal[item] === "") {
         break;
       } else {
-        ingeredientArr.push(meal[item]);
+        ingredientArr.push(meal[item]);
       }
     }
   }
@@ -36,29 +38,48 @@ const Food = () => {
     }
   }
 
-  let finalArr = [];
-  // let finalArr = ingeredientArr.concat(measureArr)
-
-  // console.log(typeof ingeredientArr,typeof  measureArr)
-
-  // console.log(typeof Object.values(ingeredientArr))
-
-  // let j = 0;
-  for(let j = 0; ingeredientArr.length > j; j++) {
-    // finalArr.push(ingeredientArr.keys());
-    finalArr(...finalArr, ingeredientArr[j])
-    j++;
+  for(let i = 0 ; i < ingredientArr.length ; i++){
+    finalArr.push(`${ingredientArr[i]} ${measureArr[i]}`);
   }
-  console.log(finalArr)
-
-  // let name = ["sara , reza , ali"]
-  // let family = ["ahmadi , mosavi ,moradi"]
-
-
+  console.log(meal)
   return (
-    <main className="main-content bg-whitesmoke">
-      <p>hi</p>
-    </main>
+    <div>
+      <main className="min-w-screen flex flex-row-reverse justify-evenly my-10">
+        <div className="w-96 flex flex-col">
+          <div className="h-10 w-56 flex justify-evenly items-center">
+            <p>{meal.strArea}</p>
+            <p>{meal.strCategory}</p>
+            <p>{meal.strTags}</p>
+          </div>
+          <p className="text-5xl font-semibold">{meal.strMeal}</p>
+          <div className="my-10">
+            {
+              finalArr.map((item , index) =>{
+                return(
+                  <div key={index}>
+                <p>{item}</p>
+              </div>
+            )})}
+          </div>
+          <p className="text-justify">Instructions: {meal.strInstructions}</p>
+          <div className="flex justify-between my-10">
+            <a href={meal.strSource}>Source</a>
+            <a href={meal.strYoutube}>Youtube</a>
+          </div>
+        </div>
+        <img className="w-96 h-96" src={meal.strMealThumb} alt={meal.idMeal} />
+      </main>
+      <div className="min-w-[90vw] h-96 bg-blue-600 my-10 flex justify-center items-center overflow-x-auto">
+          {ingredientArr.map((item, idx) => {
+            return (
+              <div key={idx} className="w-96 mx-2">
+                <img className="w-full" src={`https://www.themealdb.com/images/ingredients/${item}.png`} />
+                <p className="text-center">{item}</p>
+              </div>
+            )
+          })}
+      </div>
+    </div>
   );
 };
 
